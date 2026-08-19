d93d60e768c5753c161ca915fe0296f79c7bac69
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-08-19 14:30:37 +0000

    commit

--- EXERCICIOS/Ex02/ConversorTemp.java
+++ EXERCICIOS/Ex02/ConversorTemp.java
@@ -1,1 +1,23 @@
+package EXERCICIOS.Ex02;
+
+import java.util.Scanner;
+
+/*
+Crie um programa que solicite uma temperatura em graus Celsius ($C$) e a converta para Fahrenheit (F) utilizando a fórmula:
+F = (C * 1.8) + 32 
+ */
+
+public class ConversorTemp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite uma temperatura em Celsius: ");
+        double celsius = sc.nextDouble();
+
+        double fahrenheit = (celsius * 1.8) + 32;
+
+        System.out.println("Essa temperatura em Fahrenheit: " + fahrenheit);
+        sc.close();
+    }
+}
 
