bb5a3b45a877b32a30c2c2f774a7c1354e9f69ca
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-08-19 11:52:45 -0300

    Initialize AreaPerimetro class with package and imports
    Added package declaration and import statement for Scanner.

--- EXERCICIOS/Ex04/AreaPerimetro.java
+++ EXERCICIOS/Ex04/AreaPerimetro.java
@@ -1,1 +1,14 @@
+package EXERCICIOS.Ex04;
 
+import java.util.Scanner;
+
+/*
+Faça um programa que leia a base e a altura de um retângulo. Em seguida, calcule e mostre:
+*/
+
+public class AreaPerimetro {
+  public static void main(String[] args) {
+
+
+  }
+}
