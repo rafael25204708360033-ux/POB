222c8236c08e5b8f7af3b0ae7e3396873c72ec25
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-08 19:30:51 -0300

    Atualizar o controleLivro.java

--- Semana03/ExClass/Ex01/Controle/controleLivro.java
+++ Semana03/ExClass/Ex01/Controle/controleLivro.java
@@ -1,5 +1,19 @@
 package Semana03.ExClass.Ex01.Controle;
 
 public class controleLivro {
-    
+    public static void main(String[] args) {
+        Livro livro1 = new Livro();
+        livro1.titulo = "O Senhor dos Aneis";
+        livro1.autor = "J. R. R. Tolkien";
+        livro1.numeroPaginas = 1200;
+
+        Livro livro2 = new Livro();
+        livro2.titulo = "Dom Casmurro";
+        livro2.autor = "Machado de Assis";
+        livro2.numeroPaginas = 256;
+
+        System.out.println("=== DADOS DOS LIVROS ===");
+        livro1.exibirInformacoes();
+        livro2.exibirInformacoes();
+    }
 }
