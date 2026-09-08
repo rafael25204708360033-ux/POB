76039c5f4bd24195ab1bd393972ac9709dd4962e
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-08 19:29:57 -0300

    Atualizar o Livro.java

--- Semana03/ExClass/Ex01/Dominio/Livro.java
+++ Semana03/ExClass/Ex01/Dominio/Livro.java
@@ -1,5 +1,14 @@
 package Semana03.ExClass.Ex01.Dominio;
 
 public class Livro {
-    
+    public String titulo;
+    public String autor;
+    public int numeroPaginas;
+
+    public void exibirInformacoes() {
+        System.out.println("Titulo: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Numero de Paginas: " + numeroPaginas);
+        System.out.println("-----------------------------------");
+    }
 }
