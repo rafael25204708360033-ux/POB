2c1cbd880f71f47e8367328e7a20eeebfea2700c
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-08 19:31:36 -0300

    Atualizar o Circulo.java

--- Semana03/ExClass/Ex02/Dominio/Circulo.java
+++ Semana03/ExClass/Ex02/Dominio/Circulo.java
@@ -1,5 +1,13 @@
 package Semana03.ExClass.Ex02.Dominio;
 
 public class Circulo {
-    
+    public double raio;
+
+    public double calcularArea() {
+        return Math.PI * Math.pow(raio, 2);
+    }
+
+    public double calcularPerimetro() {
+        return 2 * Math.PI * raio;
+    }
 }
