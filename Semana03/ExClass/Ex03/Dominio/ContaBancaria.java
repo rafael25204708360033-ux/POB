6c3adb72b82a46d9bf52ba742d9a9f3371c8da7c
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-08 19:35:26 -0300

    Atualizar o ContaBancaria.java

--- Semana03/ExClass/Ex03/Dominio/ContaBancaria.java
+++ Semana03/ExClass/Ex03/Dominio/ContaBancaria.java
@@ -1,5 +1,29 @@
 package Semana03.ExClass.Ex03.Dominio;
 
 public class ContaBancaria {
-    
+    public String titular;
+    public String numeroConta;
+    public double saldo = 0.0;
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.printf("Deposito de R$ %.2f realizado com sucesso.\n", valor);
+        } else {
+            System.out.println("Valor de deposito invalido.");
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0 && saldo >= valor) {
+            saldo -= valor;
+            System.out.printf("Saque de R$ %.2f realizado com sucesso.\n", valor);
+        } else {
+            System.out.println("Saldo insuficiente ou valor invalido");
+        }
+    }
+
+    public void consultarSaldo() {
+        System.out.printf("Conta: %s | Titular: %s | Saldo atual: R$ %.2f\n", numeroConta, titular, saldo);
+    }
 }
