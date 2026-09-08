d655aee295442e852b97ba4c0a975a4b683adbf5
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-08 19:36:34 -0300

    Atualizar o controleContaBancaria.java

--- Semana03/ExClass/Ex03/Controle/controleContaBancaria.java
+++ Semana03/ExClass/Ex03/Controle/controleContaBancaria.java
@@ -1,5 +1,25 @@
 package Semana03.ExClass.Ex03.Controle;
 
+import Semana03.ExClass.Ex03.dominio.ContaBancaria
+
 public class controleContaBancaria {
-    
+    public static void main(String[] args) {
+        ContaBancaria conta = new ContaBancaria();
+        conta.titular = "Carlos Silva";
+        conta.numeroConta = "12345-6";
+
+        System.out.println("=== TESTES DE OPERACOES BANCARIAS ===");
+        conta.consultarSaldo();
+
+        conta.depositar(500.0);
+        conta.consultarSaldo();
+
+        conta.sacar(150.0);
+        conta.consultarSaldo();
+
+        conta.sacar(600.0); // Tentativa invalida: saldo insuficiente
+        conta.depositar(-50.0); // Tentativa invalida: valor negativo
+
+        conta.consultarSaldo();
+    }
 }
