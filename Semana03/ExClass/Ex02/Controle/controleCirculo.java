1240dc408c139c674a9d4b0398f2de3af31b5805
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-08 19:32:44 -0300

    Atualizar o controleCirculo.java

--- Semana03/ExClass/Ex02/Controle/controleCirculo.java
+++ Semana03/ExClass/Ex02/Controle/controleCirculo.java
@@ -1,5 +1,19 @@
 package Semana03.ExClass.Ex02.Controle;
 
+import dominio.Circulo;
+import java.util.Scanner;
+
 public class controleCirculo {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Circulo circulo = new Circulo();
+
+        System.out.print("Digite o raio do circulo: ");
+        circulo.raio = scanner.nextDouble();
+
+        System.out.printf("Area do circulo: %.2f\n", circulo.calcularArea());
+        System.out.printf("Perimetro do circulo: %.2f\n", circulo.calcularPerimetro());
+
+        scanner.close();
+    }
 }
