02246f103fbc58bdede70237802499b367eb87b0
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-08 19:37:14 -0300

    Atualizar o Funcionario.java

--- Semana03/ExClass/Ex04/Dominio/Funcionario.java
+++ Semana03/ExClass/Ex04/Dominio/Funcionario.java
@@ -1,5 +1,15 @@
 package Semana03.ExClass.Ex04.Dominio;
 
 public class Funcionario {
-    
+    public String nome;
+    public String cargo;
+    public double salarioBruto;
+
+    public void aplicarAumento(double porcentagem) {
+        salarioBruto += salarioBruto * (porcentagem / 100.0);
+    }
+
+    public double calcularSalarioLiquido(double descontoImposto) {
+        return salarioBruto - descontoImposto;
+    }
 }
