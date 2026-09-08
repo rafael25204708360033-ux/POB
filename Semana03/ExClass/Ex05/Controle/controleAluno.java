9b0ee322be2ed0a08508a668ff866269106537b1
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-08 19:40:20 -0300

    Atualizar o controleAluno.java

--- Semana03/ExClass/Ex05/Controle/controleAluno.java
+++ Semana03/ExClass/Ex05/Controle/controleAluno.java
@@ -1,5 +1,15 @@
 package Semana03.ExClass.Ex05.Controle;
 
+import Semana03.ExClass.Ex05.Dominio.Aluno;
+
 public class controleAluno {
-    
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno();
+        aluno.nome = "Lucas Ribeiro";
+        aluno.matricula = "2026CS104";
+        aluno.nota1 = 8.5;
+        aluno.nota2 = 7.0;
+
+        aluno.imprimirBoletim();
+    }
 }
