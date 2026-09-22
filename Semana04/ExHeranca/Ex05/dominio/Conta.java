884b477782152a04eafbcaeb549625f274480662
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-22 16:50:01 -0300

    Update and rename Funcionario.java to Conta.java

---
+++ Semana04/ExHeranca/Ex05/dominio/Conta.java
@@ -0,0 +1,41 @@
+package Semana04.ExHeranca.Ex05.dominio;
+
+public class Conta {
+    private String numero;
+    private double saldo;
+
+    public Conta(String numero, double saldoInicial) {
+        this.numero = numero;
+        this.saldo = saldoInicial;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    protected void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.printf("Deposito de R$ %.2f realizado. Saldo atual: R$ %.2f\n", valor, saldo);
+        }
+    }
+
+    public boolean sacar(double valor) {
+        if (valor > 0 && saldo >= valor) {
+            saldo -= valor;
+            System.out.printf("Saque de R$ %.2f realizado. Saldo atual: R$ %.2f\n", valor, saldo);
+            return true;
+        } else {
+            System.out.println("Erro: Saldo insuficiente.");
+            return false;
+        }
+    }
+}
--- Semana04/ExHeranca/Ex05/dominio/Funcionario.java
+++
@@ -1,5 +0,0 @@
-package Semana04.ExHeranca.Ex05.dominio;
-
-public class Funcionario {
-    
-}
