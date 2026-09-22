ffa0ee62837c3b9b406e446ba696ad47f133c20f
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-22 16:50:51 -0300

    Create ControleConta.java

---
+++ Semana04/ExHeranca/Ex05/controle/ControleConta.java
@@ -0,0 +1,18 @@
+package controle;
+
+import dominio.ContaCorrente;
+import dominio.ContaPoupanca;
+
+public class ControleConta {
+    public static void main(String[] args) {
+        System.out.println("=== CONTA POUPANCA ===");
+        ContaPoupanca cp = new ContaPoupanca("1001-P", 1000.0, 0.5);
+        cp.aplicarRendimento();
+        cp.sacar(200.0);
+
+        System.out.println("\n=== CONTA CORRENTE ===");
+        ContaCorrente cc = new ContaCorrente("2002-C", 100.0, 500.0);
+        cc.sacar(150.0); // Usa cheque especial e cobra R$ 2.00 de taxa
+        cc.sacar(500.0); // Tentativa que estoura o limite do cheque especial
+    }
+}
