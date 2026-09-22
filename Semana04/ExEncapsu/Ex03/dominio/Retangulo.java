2b247002ac8262b21ac63129c2c00b951f3bd9de
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-22 16:36:10 -0300

    Update Retangulo.java

--- Semana04/ExEncapsu/Ex03/dominio/Retangulo.java
+++ Semana04/ExEncapsu/Ex03/dominio/Retangulo.java
@@ -1,1 +1,45 @@
+package dominio;
 
+public class Retangulo {
+    private double largura;
+    private double altura;
+
+    public Retangulo(double largura, double altura) {
+        setLargura(largura);
+        setAltura(altura);
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        if (largura > 0.0) {
+            this.largura = largura;
+        } else {
+            System.out.println("Aviso: Largura invalida (" + largura + "). Atribuindo valor padrao: 1.0");
+            this.largura = 1.0;
+        }
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if (altura > 0.0) {
+            this.altura = altura;
+        } else {
+            System.out.println("Aviso: Altura invalida (" + altura + "). Atribuindo valor padrao: 1.0");
+            this.altura = 1.0;
+        }
+    }
+
+    public double calcularArea() {
+        return largura * altura;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * (largura + altura);
+    }
+}
