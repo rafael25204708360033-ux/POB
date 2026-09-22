542ecdc5dc3cf9ca50a84a381725156f4da9e9ce
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-22 16:47:24 -0300

    Create PushNotificacao.java

---
+++ Semana04/ExHeranca/Ex03/dominio/PushNotificacao.java
@@ -0,0 +1,13 @@
+package dominio;
+
+public class PushNotificacao extends Notificacao {
+
+    public PushNotificacao(String destinatario) {
+        super(destinatario);
+    }
+
+    @Override
+    public void enviar(String mensagem) {
+        System.out.println("Enviando Push Notification para o dispositivo " + getDestinatario() + ": " + mensagem);
+    }
+}
