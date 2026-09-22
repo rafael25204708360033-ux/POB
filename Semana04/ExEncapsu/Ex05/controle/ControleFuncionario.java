42343e39e9fe54727303df0f9eb0105e4f8362ef
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-22 16:39:05 -0300

    Create ControleFuncionario.java

---
+++ Semana04/ExEncapsu/Ex05/controle/ControleFuncionario.java
@@ -0,0 +1,22 @@
+package controle;
+
+import dominio.Funcionario;
+
+public class ControleFuncionario {
+    public static void main(String[] args) {
+        Funcionario func = new Funcionario("Beatriz Lima", "2026DEV01", 4500.0);
+        func.exibirDados();
+
+        System.out.println("--- TENTATIVA DE REDUCAO SALARIAL ---");
+        func.setSalario(4000.0);
+
+        System.out.println("\n--- TENTATIVA DE MANTER O MESMO SALARIO ---");
+        func.setSalario(4500.0);
+
+        System.out.println("\n--- AUMENTO SALARIAL VALIDO ---");
+        func.setSalario(5200.0);
+
+        System.out.println();
+        func.exibirDados();
+    }
+}
