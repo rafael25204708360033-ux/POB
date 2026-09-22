e7349de233b13bbe81c21036c78eb855b05d2701
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-22 16:48:47 -0300

    Create Quadrado.java

---
+++ Semana04/ExHeranca/Ex04/dominio/Quadrado.java
@@ -0,0 +1,14 @@
+package dominio;
+
+public class Quadrado extends FiguraGeometrica {
+    private double lado;
+
+    public Quadrado(double lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double calcularArea() {
+        return lado * lado;
+    }
+}
