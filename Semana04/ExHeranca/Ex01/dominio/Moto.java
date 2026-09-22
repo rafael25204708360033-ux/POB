47fb83f171bbae2bb14d09172657a6c0e0dccbc2
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-22 16:43:50 -0300

    Create Moto.java

---
+++ Semana04/ExHeranca/Ex01/dominio/Moto.java
@@ -0,0 +1,20 @@
+package dominio;
+
+public class Moto extends Veiculo {
+    private int cilindradas;
+
+    public Moto(String marca, String modelo, int cilindradas) {
+        super(marca, modelo);
+        this.cilindradas = cilindradas;
+    }
+
+    public int getCilindradas() {
+        return cilindradas;
+    }
+
+    @Override
+    public void exibirDetalhes() {
+        super.exibirDetalhes();
+        System.out.println("Cilindradas: " + cilindradas + " cc");
+    }
+}
