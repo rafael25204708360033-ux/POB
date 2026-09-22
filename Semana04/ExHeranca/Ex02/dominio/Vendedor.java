d487eb966ac71c679f5c1c047d9f0498f62f9e96
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-22 16:44:59 -0300

    Create Vendedor class extending Funcionario

---
+++ Semana04/ExHeranca/Ex02/dominio/Vendedor.java
@@ -0,0 +1,17 @@
+package dominio;
+
+public class Vendedor extends Funcionario {
+    private double totalVendas;
+    private double comissaoPercentual;
+
+    public Vendedor(String nome, double salarioBase, double totalVendas, double comissaoPercentual) {
+        super(nome, salarioBase);
+        this.totalVendas = totalVendas;
+        this.comissaoPercentual = comissaoPercentual;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return super.getSalarioBase() + (totalVendas * (comissaoPercentual / 100.0));
+    }
+}
