09ac0b038c463f96eea734c093cd91690b9b68a0
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-22 16:49:13 -0300

    Create Circulo.java

---
+++ Semana04/ExHeranca/Ex04/dominio/Circulo.java
@@ -0,0 +1,14 @@
+package dominio;
+
+public class Circulo extends FiguraGeometrica {
+    private double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * Math.pow(raio, 2);
+    }
+}
