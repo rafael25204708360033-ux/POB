788abc304dd67682143f8ccf3f2f9a1a3d177e0f
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-22 16:38:14 -0300

    Create ControleCarro.java

---
+++ Semana04/ExEncapsu/Ex04/controle/ControleCarro.java
@@ -0,0 +1,20 @@
+package controle;
+
+import dominio.Carro;
+
+public class ControleCarro {
+    public static void main(String[] args) {
+        Carro meuCarro = new Carro("Civic", 2024);
+
+        System.out.println("Modelo: " + meuCarro.getModelo() + " | Ano: " + meuCarro.getAno());
+        System.out.println("Carro em movimento? " + meuCarro.isEmMovimento());
+
+        System.out.println("\n--- TESTANDO ACELERACAO E FRENAGEM ---");
+        meuCarro.acelerar(50);
+        System.out.println("Carro em movimento? " + meuCarro.isEmMovimento());
+
+        meuCarro.frear(30);
+        meuCarro.frear(40); // Força a velocidade a cair abaixo de 0
+        System.out.println("Carro em movimento? " + meuCarro.isEmMovimento());
+    }
+}
