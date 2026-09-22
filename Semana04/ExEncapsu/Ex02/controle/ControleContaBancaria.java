e366d184ee83e4b6d03fd28f6b5d1fa548a68306
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-22 16:35:03 -0300

    Create ControleContaBancaria.java

---
+++ Semana04/ExEncapsu/Ex02/controle/ControleContaBancaria.java
@@ -0,0 +1,19 @@
+package controle;
+
+import dominio.ControleContaBancaria;
+
+public class ControleContaBancaria {
+    public static void main(String[] args) {
+        ContaBancaria conta = new ContaBancaria("98765-1", "Rafael Conceicao");
+
+        System.out.println("=== DADOS INICIAIS DA CONTA ===");
+        System.out.println("Numero da Conta: " + conta.getNumeroConta());
+        System.out.println("Titular: " + conta.getTitular());
+        System.out.printf("Saldo Inicial: R$ %.2f\n\n", conta.getSaldo());
+
+        System.out.println("=== OPERACOES ===");
+        conta.depositar(300.0);
+        conta.sacar(100.0);
+        conta.sacar(500.0); // Tentativa invalida
+    }
+}
