d193678cd717a823e01924a4966a95fed0f208be
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-22 16:50:31 -0300

    Create ContaCorrente.java

---
+++ Semana04/ExHeranca/Ex05/dominio/ContaCorrente.java
@@ -0,0 +1,27 @@
+package dominio;
+
+public class ContaCorrente extends Conta {
+    private double limiteChequeEspecial;
+    private static final double TAXA_SAQUE = 2.0;
+
+    public ContaCorrente(String numero, double saldoInicial, double limiteChequeEspecial) {
+        super(numero, saldoInicial);
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
+
+    @Override
+    public boolean sacar(double valor) {
+        double valorTotalNecessario = valor + TAXA_SAQUE;
+        double saldoDisponivel = getSaldo() + limiteChequeEspecial;
+
+        if (valor > 0 && saldoDisponivel >= valorTotalNecessario) {
+            setSaldo(getSaldo() - valorTotalNecessario);
+            System.out.printf("Saque de R$ %.2f (Taxa: R$ %.2f) realizado. Saldo atual: R$ %.2f\n", 
+                              valor, TAXA_SAQUE, getSaldo());
+            return true;
+        } else {
+            System.out.println("Erro: Operacao excede o limite do cheque especial disponivel.");
+            return false;
+        }
+    }
+}
