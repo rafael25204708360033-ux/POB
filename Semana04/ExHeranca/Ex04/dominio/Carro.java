079c906678c4352ff1a500176c1eaaa696cf3e8c
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-09-22 16:48:29 -0300

    Create FiguraGeometrica class with area calculation
    Added FiguraGeometrica class with calcularArea method.

--- Semana04/ExHeranca/Ex04/dominio/Carro.java
+++
@@ -1,5 +0,0 @@
-package Semana04.ExHeranca.Ex04.dominio;
-
-public class Carro {
-    
-}
---
+++ Semana04/ExHeranca/Ex04/dominio/FiguraGeometrica.java
@@ -0,0 +1,8 @@
+package Semana04.ExHeranca.Ex04.dominio;
+
+public class FiguraGeometrica {
+
+    public double calcularArea() {
+        return 0.0;
+    }
+}
